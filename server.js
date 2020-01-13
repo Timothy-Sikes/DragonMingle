5376bacc6be590b84d22284742f93b9eec1c6d58
timothy.sikes@sonicdrivein.com <timothy.sikes@sonicdrivein.com>
2020-01-12 20:26:31 -0600

    Add some dumb text.

--- server.js
+++ server.js
@@ -24,12 +24,15 @@ const compiledFunction = pug.compileFile('./content/static/PUG/home.pug');
 
 app.get('*', asyncMiddleware(async function (req, res) {
   pic = { "image": "/images/DragonLair1.jpg", "opacity" : .4 },
+  pic2 = { "image": "/images/DragonLair2.jpg", "opacity" : .4 },
 
   res.send(compiledFunction({
     catchphrase: "Meet single dragons in YOUR local area!",
     message: "A placeholder for something I might do someday maybe.",
     backgroundImage: pic.image,
     backgroundOpacity: pic.opacity || 0,
+    name2: "description1",
+    url2: pic2.image,
   }))
   })
 )
